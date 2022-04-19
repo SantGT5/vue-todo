4fff53ef440e552fe152ceeca4949558a1d600ee
SantGT5 <gianspf@gmail.com>
2022-04-19 23:10:29 +0200

    first commit && tasks file

---
+++ src/components/TaskGrid.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="task-grid">
+        <template>
+            <div v-for="(task, i) in tasks" :key="i">
+                {{ task.name }}
+            </div>
+        </template>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        tasks: {type: Array, required: true}
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
